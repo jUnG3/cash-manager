51e6f73cc2ed40acc01f03d6fb42f087911706da
jUnG3 <soldo.stjepan1304@gmail.com>
2017-01-18 21:39:52 +0100

    Add deposit test

--- tests/Transaction/TransactionTest.php
+++ tests/Transaction/TransactionTest.php
@@ -18,6 +18,19 @@ class TransactionTest extends TestCase
     }
 
     /**
+     * @param $a
+     * @param $b
+     * @param $result
+     * @dataProvider withdrawDataProvider
+     */
+    public function testDeposit($a, $b, $result)
+    {
+        $transaction = new Transaction(DataStructure::create(), new Balance(DataStructure::create($a)));
+        $transaction->withdraw($b);
+        $this->assertEquals($result, $transaction->newBalanceValue());
+    }
+
+    /**
      * @return array
      */
     public function withdrawDataProvider()
@@ -28,6 +41,20 @@ class TransactionTest extends TestCase
             'Withdraw 40 from balance' => [['current_state' => 200.00], 40.00, 160.00],
             'Withdraw 50 from balance' => [['current_state' => 200.00], 50.00, 150.00],
             'Withdraw 60 from balance' => [['current_state' => 200.00], 60.00, 140.00],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function depositDataProvider()
+    {
+        return [
+            'Deposit 20 from balance' => [['current_state' => 200.00], 20.00, 220.00],
+            'Deposit 30 from balance' => [['current_state' => 200.00], 30.00, 230.00],
+            'Deposit 40 from balance' => [['current_state' => 200.00], 40.00, 240.00],
+            'Deposit 50 from balance' => [['current_state' => 200.00], 50.00, 250.00],
+            'Deposit 60 from balance' => [['current_state' => 200.00], 60.00, 260.00],
         ];
     }
 }
